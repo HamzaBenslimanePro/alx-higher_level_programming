dc05f718dc8a8770c7f2b2d7374b89eb71bca7ac
HamzaBenslimane <hamzasamiben@gmail.com>
2024-02-06 12:48:02 -0800

    just another commit

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """load from json to file"""
-    with open(filename, encoding="utf-8") as file_loaded:
-        return json.load(file+loaded)
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
