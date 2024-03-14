0b3735b75434a72995073ca85935d2b110f60353
HamzaBenslimane <hamzasamiben@gmail.com>
2024-03-14 15:54:55 -0700

    just another commit

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,6 @@
+-- Display the top 3 cities with the highest temperature during July and August
 SELECT city, temperature
 FROM Temperatures
 WHERE month IN ('July', 'August')
 ORDER BY temperature DESC
 LIMIT 3;
-
